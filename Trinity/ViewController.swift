d42c69ef09670f02b0a0be41775309882d9c828a
Isaac Zahau <isaaclian11@gmail.com>
2020-08-18 17:12:00 -0500

    Initial Commit

---
+++ Trinity/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Trinity
+//
+//  Created by Isaac Sanga on 8/18/20.
+//  Copyright © 2020 Isaac Zahau. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
